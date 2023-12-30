65b1d9120b39b16fd4d2511a2fd700812d69a906
Surajh09 <98706142+Surajh09@users.noreply.github.com>
2023-12-30 15:32:25 +0530

    Update settings.py

--- Cloud_lab/settings.py
+++ Cloud_lab/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "hfkjwhfo3wu9ruwuhfiejfo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "True"
+DEBUG = "False"
 
 ALLOWED_HOSTS = ["cloud-lab-t8hn.onrender.com"]
 
